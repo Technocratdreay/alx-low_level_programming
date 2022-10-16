f62724d8077e9d21b356bf6fed1479f8b10efc23
Technocratdreay <olowogemotimileyin@gmail.com>
2022-10-16 03:57:16 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * Return: Always 0
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("size of a char: %i byte(s)\n", sizeof(char));
