906f3b9e45d5303085c8be2495dc40ea093037bf
Technocratdreay <olowogemotimileyin@gmail.com>
2022-10-15 17:04:34 -0700

    fight

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
-
 /**
+* main - entry point
  *
- *  * main - entry point
- *
- *   *
- *
- *    * Return: Always 0
- *
- *     */
-
+ * Return: Always 0*
+ */
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
