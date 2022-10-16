12a4965224f1e6ede7aa8183257eef435adfeff8
Technocratdreay <olowogemotimileyin@gmail.com>
2022-10-16 07:06:02 -0700

    combo variable

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0
  */
-
 int main(void)
 {
 	char low;
@@ -15,7 +13,7 @@ int main(void)
 	for (low = 'a'; low <= 'z'; low++)
 		if (low == 'q' || low == 'e')
 			continue;
-	putchar(low);
+				putchar(low);
 	putchar('\n');
 	return (0);
 }
